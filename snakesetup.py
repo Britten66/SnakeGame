9280a32fb549d83f42432b870338f30366b32e24
Chris <Britten63@hotmail.com>
2025-07-21 00:27:26 -0300

    Added Move seciton and implemented coordination, working on key binding now ..

--- snakesetup.py
+++ snakesetup.py
@@ -22,3 +22,38 @@ head.goto(0, 0)
 head.direction = "stop"
 
 #Move Section // Come Back To This ??? 
+
+#Moving with def 
+
+def UpHere ():
+    if head.direction != "down":
+        head.direction = "up" 
+
+def godown ():
+    if head.direction != "up":
+        head.direction = "down"
+    
+def LeftHere ():
+    if head.direction != "right":
+        head.direction = "leaft"
+
+def RightMove ():
+        if head.direction != "left":
+             head.direction = "right" 
+
+def move():
+        if head.direction == "up";
+                y = head.ycor() # Cool here ... coor .. nice 
+                head.sety(y+20)
+        if head.direction == "down":
+         y = head.ycor # Again very cool here ... -----= === 
+        head.sety(y-20)
+        # X now 
+        if head.direction == "left":
+             x = head.xcor()
+             head.setx(x-20)
+        if head.direction == "right":
+             x = head.xcor()
+             head.setx(x+20)
+
+    # Key Bind Here --- Imortant -- 
